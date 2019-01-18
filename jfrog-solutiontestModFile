ae070b59de05ccc0be3156c5990d45771c173674
jfrog-solutiontest <jfrog-solutions-ci@jfrog.com>
2019-01-17 23:49:01 -0800

    commit with no tags

---
+++ jfrog-solutiontestModFile
@@ -0,0 +1,6 @@
+module github.com/jfrog-solutiontest/simple
+
+require (
+	github.com/jfrog-solutiontest/dog v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20190110200230-915654e7eabc
+)
---
+++ simple.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jfrog-solutiontest/dog"
+        "golang.org/x/net/context"
+)
+
+func main () {
+	fmt.Println ("Package Name: akita")
+	dog.dog.AkitaPackageName()
+	dog.dog.ColliePackageName()
+        type Context = context.Context
+        type CancelFunc = context.CancelFunc
+}
